5d5524b3f63943c56e3d1566a5a3e3796eafaeab
Fran Lorente <fralg100@gmail.com>
2018-12-16 16:40:11 +0100

    Ejercicios acabados

---
+++ EjerciciosArrays/src/ejerciciosArray/Ejercicio09.java
@@ -0,0 +1,31 @@
+package ejerciciosArray;
+
+public class Ejercicio09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arrayA= {0,1,2,3,4,5,6,7,8,9};
+		int[] arrayB= {7,8,9};
+		int k=0;
+		boolean contenido=false;
+		
+		for (int i = 0; i < arrayA.length; i++) {
+			while(arrayB[k]==arrayA[i]) {
+				k++;
+				i++;
+				if(arrayB[k]==arrayB[arrayB.length-1] && arrayB[arrayB.length-1]==arrayA[i]) {
+					System.out.println("B está contenido en A");
+					contenido=true;
+					break;
+				}
+			}
+			k=0;
+		}
+		
+		if(!contenido)
+			System.out.println("B NO está contenido en A");
+
+	}
+
+}
