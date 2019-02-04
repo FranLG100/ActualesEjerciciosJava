3d68596ef5441dc3e9c6665e655ad73282cd1f0c
Fran <fralg100@gmail.com>
2019-02-04 13:58:12 +0100

    Mapas

---
+++ Colecciones/src/mapas/Mapas.java
@@ -0,0 +1,39 @@
+package mapas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import utilesFran.Amadeus;
+
+public class Mapas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Amadeus amadeus=new Amadeus();
+		HashMap<String,Persona> map=new HashMap<String,Persona>();
+		Persona p=null;
+		
+		p = new Persona("28823344Q", "Francisco", "Lorente", 210);
+
+		map.put(p.getDni(), p);
+
+		p = new Persona("312421321r", "Juan", "Rodríguez", 110);
+
+		map.put(p.getDni(), p);
+
+		p = new Persona("426482431Q", "Pacheca", "Domínguez", 158);
+
+		map.put(p.getDni(), p);
+		
+		for (Map.Entry<String, Persona> m: map.entrySet()) {
+			
+			String dni=m.getKey();
+			Persona pe=m.getValue();
+			
+			System.out.println("DNI:"+dni+pe.toString()+"\n");
+			
+		}
+	}
+
+}
