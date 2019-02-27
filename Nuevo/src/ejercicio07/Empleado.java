b1f097dc02871d8be633a80980d8830f59fded1a
Fran <fralg100@gmail.com>
2019-02-27 11:10:53 +0100

    Comienzo ejercicio 07

---
+++ Nuevo/src/ejercicio07/Empleado.java
@@ -0,0 +1,62 @@
+package ejercicio07;
+
+public abstract class Empleado {
+	
+	private String primerNombre, apellidoPaterno;
+	private String nss;
+	
+	
+	public Empleado(String primerNombre, String apellidoPaterno, String nss) {
+		this.primerNombre = primerNombre;
+		this.apellidoPaterno = apellidoPaterno;
+		this.nss = nss;
+	}
+	
+	
+	
+	public abstract void ingresos();
+
+	@Override
+	public String toString() {
+		return "\nNombre: "+primerNombre+"\nApellido: "+apellidoPaterno+"\nNúmero de la Seguridad Social: "+nss;
+	}
+
+
+	public String getPrimerNombre() {
+		return primerNombre;
+	}
+
+
+
+	public void setPrimerNombre(String primerNombre) {
+		this.primerNombre = primerNombre;
+	}
+
+
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+
+
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+
+
+
+	public String getNss() {
+		return nss;
+	}
+
+
+
+	public void setNss(String nss) {
+		this.nss = nss;
+	}
+	
+	
+	
+	
+}
