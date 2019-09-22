af52b7bb83fd1bcc2bab6cfefd220613bdcdf3ac
fralg100@gmail.com <fralg100@gmail.com>
2019-09-22 10:52:06 +0200

    Plantilla MVC

---
+++ PlantillaMVC/src/controlador/Main.java
@@ -0,0 +1,12 @@
+package controlador;
+
+import vista.Vista;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 new Controlador(new Vista()).iniciar();
+	}
+
+}
